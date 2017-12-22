6859264f216b253df38faa7e39edf0adb275adfd
Anonymous Applicant <anonymous@example.com>
2017-12-22 11:40:14 -0500

    Wired up end-to-end

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,10 +1,42 @@
 (ns my-exercise.search
-  (:require [hiccup.page :refer [html5]]))
+  (:require [hiccup.page :refer [html5]]
+            [my-exercise.ocd :as ocd]
+            [my-exercise.turbovote :as turbovote]))
+
+(defn header
+  "TODO: move me to shared ns and update home.clj"
+  [title]
+  [:head
+   [:meta {:charset "UTF-8"}]
+   [:meta {:name "viewport"
+           :content "width=device-width, initial-scale=1.0, maximum-scale=1.0"}]
+   [:title title]
+   [:link {:rel "stylesheet" :href "default.css"}]])
+
+(defn extract-address
+  "Perform translation between form and what OCD expects"
+  [params]
+  params)
+
+(defn render-election
+  [election]
+  [:div.election
+   [:span.date (:date election)]
+   [:span.description (:description election)]])
+
+(defn render-response
+  [elections]
+  (html5
+   (header "Upcoming Elections")
+   [:body
+    [:section#summary (str (count elections) " results found")]
+    [:section#results (map render-election elections)]]))
 
 (defn search
   "Handler for election search. Expects address params"
   [{:keys [params] :as request}]
-  (html5
-   [:body
-    [:section.summary "No results found"]
-    [:section.results (pr-str params)]]))
+  (-> params
+      extract-address
+      ocd/all-ids
+      turbovote/upcoming-elections
+      render-response))
--- src/my_exercise/turbovote.clj
+++ src/my_exercise/turbovote.clj
@@ -7,7 +7,8 @@
   divisions - a collection of OCD-ids
   returns - seq of maps containing election data"
   [divisions]
-  (let [options {:query-params {:district-divisions (st/join "," divisions)}}
+  (let [options {:as :text
+                 :query-params {:district-divisions (st/join "," divisions)}}
         request (http/get "https://api.turbovote.org/elections/upcoming" options)
         {:keys [body error]} @request]
     (clojure.edn/read-string body)))

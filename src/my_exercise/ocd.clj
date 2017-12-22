d2c2b5fb6726e0d36df6f182caa34f255631d307
Anonymous Applicant <anonymous@example.com>
2017-12-22 10:50:16 -0500

    OCD formatting and generation

--- src/my_exercise/ocd.clj
+++ src/my_exercise/ocd.clj
@@ -5,20 +5,30 @@
   "Return true if input not nil and not empty string"
   (complement st/blank?))
 
+(defn format
+  "strip leading/trailing spaces, downcase, and underscore words
+  TODO: might be able to consolidate leading/trailing string replace"
+  [s]
+  (-> s
+      (st/replace #"^\s+" "")
+      (st/replace #"\s+$" "")
+      (st/replace #"\s+" "_")
+      st/lower-case))
+
 (defn id
   "Return the longest possible OCD identifier given the input map"
-  [{:keys [country county state place]}]
+  [{:keys [country county state place] :or {country "us"}}]
   (cond-> "ocd-division"
-    (present? country) (str "/country:" country)
-    (present? state) (str "/state:" state)
-    (present? county) (str "/county:" county)
-    (present? place) (str "/place:" place)))
+    (present? country) (str "/country:" (format country))
+    (present? state) (str "/state:" (format state))
+    (present? county) (str "/county:" (format county))
+    (present? place) (str "/place:" (format place))))
 
 (defn all-ids
   "Return a collection of all OCD identifier strings that can be generated from the input.
   TODO: normalize address info (e.g. zip => county, city => place)"
   [address]
-  [(id (select-keys address [:country]))
-   (id (select-keys address [:country :state]))
-   (id (select-keys address [:country :state :county]))
-   (id (select-keys address [:country :state :place]))])
+  (distinct [(id (select-keys address [:country]))
+             (id (select-keys address [:country :state]))
+             (id (select-keys address [:country :state :county]))
+             (id (select-keys address [:country :state :place]))]))
--- test/my_exercise/ocd_test.clj
+++ test/my_exercise/ocd_test.clj
@@ -8,4 +8,19 @@
             "ocd-division/country:us/state:al"
             "ocd-division/country:us/state:al/county:jefferson"
             "ocd-division/country:us/state:al/place:birmingham"]
-           (ocd/all-ids {:country "us" :state "al" :county "jefferson" :place "birmingham"})))))
+           (ocd/all-ids {:country "us" :state "al" :county "jefferson" :place "birmingham"}))))
+  (testing "sparse example"
+    (is (= ["ocd-division/country:us"
+            "ocd-division/country:us/state:al"
+            "ocd-division/country:us/state:al/place:birmingham"]
+           (ocd/all-ids {:country "us" :state "al" :place "birmingham"}))))
+  (testing "formatting example"
+    (is (= ["ocd-division/country:us"
+            "ocd-division/country:us/state:al"
+            "ocd-division/country:us/state:al/place:pleasant_hill"]
+           (ocd/all-ids {:country "US" :state "AL" :place " Pleasant Hill "}))))
+  (testing "default to US"
+    (is (= ["ocd-division/country:us"
+            "ocd-division/country:us/state:al"]
+           (ocd/all-ids {:state "AL"}))))
+  )
